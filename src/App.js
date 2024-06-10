dbd4c8f40567b85ef1af89b88e286d288010ccd9
Sankaranarayanankm <sankaranarayanan.km97@gmail.com>
2024-06-10 16:14:09 +0530

    added functionality to get data from backend when page loads

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import { useDispatch, useSelector } from "react-redux";
 import Notification from "./components/UI/Notification";
 import { uiAction } from "./Store/uiSlice";
+import { cartActions } from "./Store/cartSlice";
 let initial = true;
 function App() {
   const showCart = useSelector((state) => state.ui.showCart);
   const cart = useSelector((state) => state.cart);
   const notification = useSelector((state) => state.ui.notification);
   const dispatch = useDispatch();
-  console.log(notification);
+
+  useEffect(() => {
+    async function getData() {
+      try {
+        const response = await fetch(
+          "https://practice-b7928-default-rtdb.firebaseio.com/cart.json"
+        );
+        if (!response.ok) {
+          const errData = await response.json();
+          throw new Error(errData.error.message || "Failed to get Data");
+        }
+        const resData = await response.json();
+        console.log(resData.items);
+        for (let val in resData.items) {
+          dispatch(cartActions.addToCart(resData.items[val]));
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getData();
+  }, []);
 
   useEffect(() => {
     const storeData = async () => {
